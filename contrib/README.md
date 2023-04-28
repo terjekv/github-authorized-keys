30507b213b0e05ad91ca1cefb59734a867d24759
Terje Kvernes <terjekv@users.noreply.github.com>
2023-04-28 09:15:18 +0200

    Terjekv/issue-38-Resolve-policy-issues-on-RHEL-<-9 (#40)
    * Rewrite of the installation script.
    
      - Resolves policy issues on RHEL < 9
      - Tested on Rocky8, Rocky9, Ubuntu 22, and also on arm.
      - Default is to not ask for input, but read ENV.
    
    Fixes terjekv/github-authorized-keys#38
    Fixes terjekv/github-authorized-keys#39

--- contrib/README.md
+++ contrib/README.md
@@ -4,11 +4,38 @@ Bits and pieces that are helpful for getting github-authorized-keys up and runni
 
 ## Install.sh
 
-A simple script that installs github-authorized-keys to /usr/local/sbin and sets up a
-systemd service file. It also installs the SELinux policy files if SELinux is detected.
-For most installations, this is very simple solution to getting github-authorized-keys
-up and running.
+A script that installs github-authorized-keys to /usr/local/sbin and sets up a systemd service file. It also installs the SELinux policy file and sets up file contexts if SELinux is detected.
 
+For most installations, this is very simple solution to getting github-authorized-keys up and running.
+
+Note that SELinux support for this script is limited to `SE Linux modular policy version 1, mod version 19`. This should be fine for RHEL 8 and newer derivatives, but may not work on older systems (RHEL 7 and friends). If you are running an older system, you can still use the script, but you will have to modify it to use your own policy file (see below).
+
+A typical way to call the script is to do:
+
+```bash
+GAK_GITHUB_API_TOKEN="ghp_..." GAK_GITHUB_ORGANIZATION="myorgname" GAK_GITHUB_ADMIN_TEAM_NAME="myadminteam" /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/terjekv/github-authorized-keys/main/contrib/install.sh)"
+```
+
+If you want to give the values interactively, you can do:
+
+```bash
+GAK_INTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/terjekv/github-authorized-keys/main/contrib/install.sh)"
+```
+
+You may also combine the two:
+
+```bash
+GAK_INTERACTIVE=1 GAK_GITHUB_API_TOKEN="ghp_..." GAK_GITHUB_ORGANIZATION="myorgname" /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/terjekv/github-authorized-keys/main/contrib/install.sh)"
+```
+
+When issuing interactive usage, the defaults the script provides will be the same as it would have chosen if the script was run non-interactively.
+The order of precedence for values are as follows:
+
+1. Existing values from an already existing configuration file
+2. Environment variables mapped to variables in the script, prefixed with `GAK_`
+3. Values from the environment file selected for the platform (rhel/debian)
+
+Any value issued interactively will be selected above the defaults.
 
 
 ## Environment files
@@ -29,14 +56,25 @@ run from /usr/local/sbin, which on some systems requires a custom SELinux policy
 
 ## SELinux
 
-There are two policy files available:
+### File contexts
 
-- [my-curl.pp](my-curl.pp): This policy allows curl to access the github API when called as
-  a part of sshd.
+The following file contexts are required for github-authorized-keys to run:
 
-- [allow-github-authorized-keys-from-usr-local.pp](allow-github-authorized-keys-from-usr-local.pp):
-  This policy allows github-authorized-keys to be executed by systemd while located in /usr/local/sbin.
+- `/usr/local/sbin/github-authorized-keys`: `bin_t`
+- `/usr/local/sbin/authorized-keys`: `bin_t`
 
-For default installations on SELinux systems, the first is required. You can avoid the latter if you
-install github-authorized-keys to /usr/sbin instead of /usr/local/sbin. However, this will require 
-a number of manual steps to get the service file set up correctly.
+`install.sh` will install these file contexts if SELinux is detected, by running `semanage fcontext` and `restorecon`. Existing contexts for these two files will be replaced.
+
+### Policies
+
+There is a default policy file available:
+
+- [github-authorized-keys-allow-sshd-reserved-ports.pp](github-authorized-keys-allow-sshd-reserved-ports.pp):
+  This policy allows curl access to reserved ports when run by sshd.
+
+- [github-authorized-keys-allow-sshd-reserved-ports.te](github-authorized-keys-allow-sshd-reserved-ports.te):
+  This is the type enforcement file that can be used to compile the policy file. This is useful if you want
+  to ensure that you are installing a policy file that is compatible with your system. However, you will have to
+  modify install.sh to use your self-complied policy file.
+
+
--- contrib/install.sh
+++ contrib/install.sh
@@ -2,149 +2,296 @@
 
 set -e
 
-if [ "$(id -u)" -ne 0 ]; then
-        echo 'This script must be run by root' >&2
-        exit 1
-fi
+declare -A env_vars
+declare -a env_order
 
-GAK_VERSION=0.0.25
-BRANCH="${1:-main}"
-
-[ -f /etc/os-release ] && source /etc/os-release
+shopt -s nocasematch
 
 cd /tmp
+
+GAK_VERSION=0.0.25
+UNAME_ARCH=$(uname -m)
+BRANCH="${1:-main}"
+
 CURL="curl -sSfOL"
 BINARY_PATH="/usr/local/sbin"
 # Loading a given configuration file from --config doesn't work, so we use the hardcoded default
 # path for the configuration file...
-CONF_FILE="/root/.github-authorized-keys.yaml" 
-SELINUX_POLICIES="my-curl.pp allow-github-authorized-keys-from-usr-local.pp"
+CONF_FILE="$HOME/.github-authorized-keys.yaml" 
+SELINUX_POLICIES="github-authorized-keys-allow-sshd-reserved-ports.pp"
 
 # https://raw.githubusercontent.com/terjekv/github-authorized-keys/main/contrib/
 RAW_CONTRIB_URL=https://raw.githubusercontent.com/terjekv/github-authorized-keys/${BRANCH}/contrib
 
-# Fetch shared artifacts
-$CURL "https://github.com/terjekv/github-authorized-keys/releases/download/v${GAK_VERSION}/github-authorized-keys-v${GAK_VERSION}-linux-amd64.tar.gz"
-$CURL "${RAW_CONTRIB_URL}/authorized-keys"
+# Default environment file, is overridden by install_system_specific_artifacts
+ENV_FILE=env
 
-# Fetch systemd service file
-$CURL "${RAW_CONTRIB_URL}/github-authorized-keys.service"
+header() {
+    echo
+    echo "** ${1} **"
+    echo 
+}
 
-echo
-echo "Installing github-authorized-keys v${GAK_VERSION}..."
-echo
-echo "  - Detected OS: ${PRETTY_NAME}"
+display() {
+    # space before the string is $2 * 2 spaces, minimum 2
+    printf "%$(( ${2:-1} * 2 ))s - %s\n" " " "${1}"
+}
 
-if [ "${ID_LIKE}" == "fedora" ]; then
-    echo "  - System is Fedora-like, expecting systemd and SELinux."
-    echo "  - Configuring SELinux policies."
-    $CURL "${RAW_CONTRIB_URL}/env.rhel"
+load_env_vars() {
+    ENV_FILE=$1
+    while IFS="=" read -r key value; do
+        if [[ ! "${key}" =~ ^# ]] && [[ -n "${key}" ]]; then
+            value="${value%\"}"
+            value="${value#\"}"
+            env_vars["${key}"]="${value}"
+            default_env_vars["${key}"]="${value}"
+            env_order+=("${key}")
+        fi
+    done < "${ENV_FILE}"
 
-    # SElinux
-    for policy in $SELINUX_POLICIES; do
-        $CURL "${RAW_CONTRIB_URL}/${policy}"
-        semodule -i ${policy}
-        rm ${policy}
+    if [[ -f ${CONF_FILE} ]]; then
+        while IFS=": " read -r key value; do
+            if [[ ! "${key}" =~ ^# ]] && [[ -n "${key}" ]]; then
+                key="${key// /}"
+                value="${value%\"}"
+                value="${value#\"}"
+                env_vars["${key}"]="${value}"
+            fi
+        done < ${CONF_FILE}
+    fi
+}
+
+get_default_value() {
+    local token=$1
+    local vname="GAK_${token}"
+    local val=${!vname}
+
+    if [[ -z "${val}" ]]; then
+        val=${env_vars["${token}"]}
+    fi
+
+    echo "${val}"
+}
+
+print_and_confirm() {
+    local output=""
+    for token in "${env_order[@]}"; do
+        val=$(get_default_value "${token}")
+
+        if [[ -n "${GAK_INTERACTIVE}" ]]; then
+            read -p "${token} [${val}]: " interactive_val
+            if [[ -n "${interactive_val}" ]]; then
+                val=${interactive_val}
+            fi
+            # Update the value in env_vars for the next run
+            env_vars["${token}"]="${val}"
+        fi
+
+        output+="${token}: \"${val}\""$'\n'
     done
 
-    ENV_FILE=env.rhel
-else
-    echo "  - No specific setup available, assuming debian-like with systemd but no SELinux."
-
-    $CURL -OL "${RAW_CONTRIB_URL}/env"
-    ENV_FILE=env
-fi
-
-# Unpack binary
-tar xzf github-authorized-keys-v${GAK_VERSION}-linux-amd64.tar.gz
-rm github-authorized-keys-v${GAK_VERSION}-linux-amd64.tar.gz README.md LICENSE
-
-echo "  - Installing binary files into ${BINARY_PATH}."
-# Move artifacts into place
-mv github-authorized-keys ${BINARY_PATH}/github-authorized-keys
-
-mv authorized-keys ${BINARY_PATH}/authorized-keys
-
-echo "  - Installing systemd service."
-mv github-authorized-keys.service /etc/systemd/system/github-authorized-keys.service
-
-
-if test -f ${CONF_FILE} && ! grep -Fq 'ghp_token' ${CONF_FILE}; then
-    echo "  - NOTE: ${CONF_FILE} already has configuration, staying clear."
-    ENV_ALREADY_SET=True
-else
-    echo "  - Installing a fresh configuration into ${CONF_FILE}."
-    # YAMLIFY the environment file
-    echo "---" > ${ENV_FILE}.yaml
-    awk '{sub(/=/,": ");}1' < ${ENV_FILE} >> ${ENV_FILE}.yaml
-    mv ${ENV_FILE}.yaml ${CONF_FILE}
-    rm ${ENV_FILE}
-fi
-
-# Ensure permissions are correct
-chmod 755 ${BINARY_PATH}/authorized-keys
-chmod 755 ${BINARY_PATH}/github-authorized-keys
-
-systemctl daemon-reload
-
-if [ "${ID_LIKE}" == "fedora" ]; then
-    echo "  - Ensuring SELinux permissions are correct."
-    /sbin/restorecon -v ${BINARY_PATH}/github-authorized-keys > /dev/null
-    /sbin/restorecon -v ${BINARY_PATH}/authorized-keys > /dev/null
-fi
-
-echo "  - Ensuring default group exists."
-if ! grep -Fq ':999:' /etc/group; then
-    groupadd -g 999 ssh_keys
-fi
-
-echo "  - Validating ssh configuration."
-
-if ! grep -Eq '^AuthorizedKeysCommand' /etc/ssh/sshd_config; then
-    echo "    - Adding AuthorizedKeysCommand to sshd_config."
-    echo "AuthorizedKeysCommand ${BINARY_PATH}/authorized-keys" >> /etc/ssh/sshd_config 
-else
-    echo "    - AuthorizedKeysCommand already set up, skipping."
-fi
-
-if ! grep -Eq '^AuthorizedKeysCommandUser' /etc/ssh/sshd_config; then
-    echo "    - Adding AuthorizedKeysCommandUser to sshd_config."
-    echo "AuthorizedKeysCommandUser root" >> /etc/ssh/sshd_config
-else
-    echo "    - AuthorizedKeysCommandUser already set up, skipping."
-fi
-
-# https://github.com/widdix/aws-ec2-ssh/issues/157
-echo "  - Ensuring that ec2-instance-connect is not installed."
-[ -f /usr/bin/apt-get ] && apt-get -qq remove ec2-instance-connect
-
-echo
-echo "** Installation complete! **"
-echo
-
-if ! [ "${ENV_ALREADY_SET}" == "True" ]; then
-    echo "You need to configure the service before it can be used!"
-    if [ "${ID_LIKE}" == "fedora" ]; then
-        echo "Edit ${CONF_FILE} to add your token, organization, and team".    
-    else
-        echo "Edit ${CONF_FILE} to configured your setup."
-        echo "Ensure that token, organization, and team is set up, and that".
-        echo "the templates for creating users and groups are correct for your system."
+    if [[ -n "${GAK_INTERACTIVE}" ]]; then
+        echo "---------- configuration ------------"
+        printf "%s" "${output}"
+        echo "-------------------------------------"
+        read -p "Does this look okay? (yes/ok/enter to confirm) " confirm
+        if [[ ! "${confirm}" =~ ^(yes|ok)?$ ]]; then
+            print_and_confirm
+            return
+        fi
     fi
-    echo "After that run the following commands to enable and start the service".
 
-    echo systemctl enable github-authorized-keys
-    echo systemctl start github-authorized-keys
-    echo systemctl restart sshd
-else
-    echo "Configuration is already set up, enabling and starting the service, restarting sshd."
-
-    systemctl enable github-authorized-keys
-    if systemctl --type=service --state=active | grep -Fq "github-authorized-keys.service"; then
-        systemctl restart github-authorized-keys
-    else
-        systemctl start github-authorized-keys
+    local file_operation="created"
+    if [[ -f ${CONF_FILE} ]]; then
+        file_operation="updated"
     fi
-    systemctl restart sshd
-fi
 
+    echo "---" > ${CONF_FILE}
+    printf "%s" "${output}" >> ${CONF_FILE}
+    display "${CONF_FILE} has been ${file_operation}..." 2
+}
+
+fix_selinux_contexts() {
+    if [[ "${ID_LIKE}" =~ "fedora" ]]; then
+        display "Ensuring SELinux contexts are correct."
+
+        # A note on the code below.
+        # We use grep > /dev/null rather than grep -q to prevent
+        # BrokenPipeError: [Errno 32] Broken pipe
+        # as a result of the pipe being closed by grep on the first hit.
+        for binary in authorized-keys github-authorized-keys; do
+            display "${BINARY_PATH}/${binary}" 2
+            # If a policy exists for the binary, ensure we have the latest version by
+            # first removing it...
+            /usr/sbin/semanage fcontext -l | grep ${BINARY_PATH}/${binary} > /dev/null && \
+                /usr/sbin/semanage fcontext -d ${BINARY_PATH}/${binary}
+            # ...then adding the correct one...
+            /usr/sbin/semanage fcontext -a -t bin_t ${BINARY_PATH}/${binary}
+            # ...and finally relabeling the binary.
+            /usr/sbin/restorecon ${BINARY_PATH}/${binary}
+        done
+    #    chcon system_u:object_r:bin_t:s0 ${BINARY_PATH}/authorized-keys ${BINARY_PATH}/github-authorized-keys
+    fi
+}
+
+validate_environment() {
+    if [ "$(id -u)" -ne 0 ]; then
+            echo 'This script must be run by root' >&2
+            exit 1
+    fi
+
+    if [ "${UNAME_ARCH}" == "x86_64" ]; then
+        ARCH="amd64"
+    elif [ "${UNAME_ARCH}" == "aarch64" ]; then
+        ARCH="arm64"
+    else
+        echo "Unsupported architecture: ${UNAME_ARCH}"
+        exit 1
+    fi
+}
+
+fetch_artifacts() {
+    # Fetch shared artifacts
+    $CURL "https://github.com/terjekv/github-authorized-keys/releases/download/v${GAK_VERSION}/github-authorized-keys-v${GAK_VERSION}-linux-${ARCH}.tar.gz"
+    $CURL "${RAW_CONTRIB_URL}/authorized-keys"
+
+    # Fetch systemd service file
+    $CURL "${RAW_CONTRIB_URL}/github-authorized-keys.service"
+}
+
+preamble() {
+    [ -f /etc/os-release ] && source /etc/os-release
+
+    header "Installing github-authorized-keys v${GAK_VERSION}..."
+    display "Detected OS: ${PRETTY_NAME} on ${ARCH}"
+}
+
+install_system_specific_artifacts() {
+    if [[ "${ID_LIKE}" =~ "fedora" ]]; then
+        display "System is Fedora-like, expecting systemd and SELinux."
+        display "Installing SELinux policies."
+
+        # SElinux
+        for policy in $SELINUX_POLICIES; do
+            display "Installing ${policy}" 2
+            $CURL "${RAW_CONTRIB_URL}/${policy}"
+            semodule -i ${policy}
+            rm ${policy}
+        done
+
+        $CURL "${RAW_CONTRIB_URL}/env.rhel"
+        ENV_FILE=env.rhel
+    else
+        display "No specific setup available, assuming debian-like with systemd but no SELinux."
+
+        $CURL "${RAW_CONTRIB_URL}/env"
+        ENV_FILE=env
+    fi
+}
+
+install_generic_artifacts() {
+    # Unpack binary
+    tar xzf github-authorized-keys-v${GAK_VERSION}-linux-${ARCH}.tar.gz
+    rm github-authorized-keys-v${GAK_VERSION}-linux-${ARCH}.tar.gz README.md LICENSE
+
+    display "Installing binary files into ${BINARY_PATH}."
+    # Move artifacts into place
+    for file in authorized-keys github-authorized-keys; do
+        display "Installing ${file}." 2
+        mv ${file} ${BINARY_PATH}/
+    done
+
+    display "Installing systemd service."
+    mv github-authorized-keys.service /etc/systemd/system/github-authorized-keys.service
+}
+
+fix_binary_permissions() {
+    display "Fixing permissions for binaries."
+    for binary in authorized-keys github-authorized-keys; do
+        display "${BINARY_PATH}/${binary}." 2
+        chown root:root ${BINARY_PATH}/${binary}
+        chmod 755 ${BINARY_PATH}/${binary}
+    done
+}
+
+create_configuration_file() {
+    display "Creating configuration file ${CONF_FILE}."
+    load_env_vars $ENV_FILE
+    print_and_confirm
+}
+
+ensure_ssh_keys_group() {
+    display "Ensuring default group exists."
+    if ! grep -Fq ':999:' /etc/group; then
+        groupadd -g 999 ssh_keys
+    fi
+}
+
+validate_ssh_configuration() {
+    ensure_ssh_keys_group
+
+    display "Validating ssh configuration."
+
+    if ! grep -Eq '^AuthorizedKeysCommand' /etc/ssh/sshd_config; then
+        display "Adding AuthorizedKeysCommand to sshd_config." 2
+        echo "AuthorizedKeysCommand ${BINARY_PATH}/authorized-keys" >> /etc/ssh/sshd_config 
+    else
+        display "AuthorizedKeysCommand already set up, skipping." 2
+    fi
+
+    if ! grep -Eq '^AuthorizedKeysCommandUser' /etc/ssh/sshd_config; then
+        display "Adding AuthorizedKeysCommandUser to sshd_config." 2
+        echo "AuthorizedKeysCommandUser root" >> /etc/ssh/sshd_config
+    else
+        display "AuthorizedKeysCommandUser already set up, skipping." 2
+    fi
+
+    # https://github.com/widdix/aws-ec2-ssh/issues/157
+    display "Ensuring that ec2-instance-connect is not installed."
+    if [ -f /usr/bin/apt-get ]; then
+        /usr/bin/apt-get -qq remove ec2-instance-connect > /dev/null
+    fi
+}
+
+finish() {
+    config_token=$(grep -E '^GITHUB_API_TOKEN' $CONF_FILE | awk -F: '{print $2}' | cut -f2 -d'"')
+    if [ "${config_token}" == "${default_env_vars[GITHUB_API_TOKEN]}" ]; then
+        display "Default github token found in config file, not starting services."
+    else
+        sctl="/usr/bin/systemctl --quiet"
+        display "Reloading systemd daemon and enabling/restarting services."
+        $sctl daemon-reload
+
+        $sctl enable github-authorized-keys
+        if /usr/bin/systemctl --type=service --state=active | grep -Fq "github-authorized-keys.service"; then
+            $sctl restart github-authorized-keys
+        else
+            $sctl start github-authorized-keys
+        fi
+        $sctl restart sshd
+    fi
+
+    header "Installation complete!"
+}
+
+#
+# Start of script
+#
+
+validate_environment
+
+preamble
+
+fetch_artifacts
+
+install_system_specific_artifacts
+install_generic_artifacts
+
+fix_binary_permissions
+fix_selinux_contexts
+
+validate_ssh_configuration
+
+create_configuration_file
+
+finish
